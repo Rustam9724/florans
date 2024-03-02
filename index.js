516ab12119800dcbe7602a9dfefb05211f75d869
rustam_9724 <rustam_9724@mail.ru>
2024-03-02 12:01:07 +0500

    init: promotions section adaptive

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+// ---- СЛАЙДЕР В СЕКЦИИ АКЦИИ ------- ..
+let slidesCount;
+if (window.innerWidth > 1149) {
+    slidesCount = 3;
+} else if (window.innerWidth > 774 && window.innerWidth <= 1149) {
+    slidesCount = 2;
+} else {
+    slidesCount = 1;
+}
+
 new Swiper('.promotions-slider', {
     navigation: {
         nextEl: '.promotions__arrow._left',
         prevEl: '.promotions__arrow._right',
     },
-    slidesPerView: 3,
+    slidesPerView: slidesCount,
+    slidesPerGroup: slidesCount,
     spaceBetween: 20,
-    slidesPerGroup: 3,
     pagination: {
         el: '.promotions__pagination',
         clickable: true,
